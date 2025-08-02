9860a050db69c5cc6ca0213ddde21ece15fde686
Jasur <j31548359@gmail.com>
2025-08-02 14:27:05 +0500

    ui

--- admin_client/src/pages/ProductAdd.jsx
+++ admin_client/src/pages/ProductAdd.jsx
@@ -1,12 +1,29 @@
 import { useRef, useState } from "react";
+import {
+  Upload,
+  Form,
+  Input,
+  InputNumber,
+  Select,
+  Button,
+  Space,
+  message,
+  Checkbox,
+} from "antd";
 import { FaX } from "react-icons/fa6";
 import { useCreateProductMutation } from "../context/services/product.service";
+import { PlusOutlined } from "@ant-design/icons";
+
+const { TextArea } = Input;
 
 const ProductAdd = () => {
   const [additionals, setAdditionals] = useState([]);
   const [newAdditional, setNewAdditional] = useState("");
+  const [fileList, setFileList] = useState([]);
+  const [showNutrition, setShowNutrition] = useState(true);
+
   const [createProduct] = useCreateProductMutation();
-  const imageRef = useRef();
+  const formRef = useRef();
 
   const handleAddAdditional = () => {
     if (newAdditional.trim()) {
@@ -19,128 +36,262 @@ const ProductAdd = () => {
     setAdditionals(additionals.filter((_, i) => i !== index));
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const form = e.target;
-    const formData = new FormData(form);
+  const handleFileChange = ({ fileList }) => {
+    setFileList(fileList);
+  };
+
+  const onFinish = async (values) => {
+    const formData = new FormData();
+
+    Object.entries(values).forEach(([key, value]) => {
+      if (
+        typeof value === "object" &&
+        value !== null &&
+        !Array.isArray(value)
+      ) {
+        // Agar showNutrition false bo‘lsa, nutritional_value ni kiritmaymiz
+        if (key === "nutritional_value" && !showNutrition) return;
+
+        Object.entries(value).forEach(([subKey, subValue]) => {
+          formData.append(`${key}.${subKey}`, subValue);
+        });
+      } else {
+        formData.append(key, value);
+      }
+    });
 
     additionals.forEach((item) => formData.append("additionals[]", item));
-
-    // const files = imageRef.current.files;
-    // for (let i = 0; i < files.length; i++) {
-    //   formData.append("image_log", files[i]);
-    // }
+    fileList.forEach((file) => {
+      formData.append("image_log", file.originFileObj);
+    });
 
     try {
-      const res = await createProduct(formData).unwrap();
-      console.log(res);
-
-      alert("Mahsulot muvaffaqiyatli qo'shildi");
+      await createProduct(formData).unwrap();
+      message.success("Mahsulot muvaffaqiyatli qo'shildi");
+      formRef.current.resetFields();
+      setAdditionals([]);
+      setFileList([]);
+      setShowNutrition(true);
     } catch (err) {
       console.error("Xatolik:", err);
+      message.error("Xatolik yuz berdi");
     }
   };
 
   return (
     <div className="product-add">
-      <form
-        onSubmit={handleSubmit}
-        autoComplete="off"
-        encType="multipart/form-data"
-      >
-        <p>Tovar nomi</p>
-        <input type="text" name="product_name" />
-        <p>Tovar tavsifi</p>
-        <textarea name="product_description" rows="5" cols="10"></textarea>
-        <p>Tovar tarkibi</p>
-        <textarea name="product_ingredients" rows="5" cols="10"></textarea>
-        <br />
-        <div>
-          <div>
-            <p>Tovar birligi</p>
-            <select name="unit">
-              <option value="dona">Dona</option>
-              <option value="gr">Kilogramm</option>
-              <option value="litr">Litr</option>
-              <option value="sm">Santimetr</option>
-            </select>
+      <div className="product-add-form">
+        <Form
+          layout="vertical"
+          onFinish={onFinish}
+          ref={formRef}
+          autoComplete="off"
+          style={{ display: "flex", gap: "15px" }}
+        >
+          <div className="product-add-grid">
+            <Form.Item
+              label="Tovar nomi"
+              name="product_name"
+              rules={[{ required: true }]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item label="Tovar tavsifi" name="product_description">
+              <TextArea rows={3} />
+            </Form.Item>
+
+            <Form.Item label="Tovar tarkibi" name="product_ingredients">
+              <TextArea rows={3} />
+            </Form.Item>
+
+            <Form.Item
+              label="Tovar birligi va tavsifi"
+              style={{ gridColumn: "span 2" }}
+            >
+              <div style={{ display: "flex", gap: 16, width: "100%" }}>
+                <Form.Item name="unit" rules={[{ required: true }]} noStyle>
+                  <Select placeholder="Tovar birligi" style={{ width: "100%" }}>
+                    <Select.Option value="dona">Dona</Select.Option>
+                    <Select.Option value="gr">Kilogramm</Select.Option>
+                    <Select.Option value="litr">Litr</Select.Option>
+                    <Select.Option value="sm">Santimetr</Select.Option>
+                  </Select>
+                </Form.Item>
+
+                <Form.Item
+                  name="unit_description"
+                  rules={[{ required: true }]}
+                  noStyle
+                >
+                  <Input
+                    placeholder="Birlik tavsifi"
+                    style={{ width: "100%" }}
+                  />
+                </Form.Item>
+
+                <Form.Item
+                  name="expiration"
+                  rules={[{ type: "number", min: 0 }]}
+                  noStyle
+                >
+                  <InputNumber
+                    placeholder="Yaroqlilik kunlari"
+                    style={{ width: "100%" }}
+                  />
+                </Form.Item>
+              </div>
+            </Form.Item>
+
+            <Form.Item label="Sotish narxi" name="selling_price">
+              <InputNumber min={0} style={{ width: "100%" }} />
+            </Form.Item>
+
+            <Form.Item label="Kategoriya" style={{ gridColumn: "span 2" }}>
+              <div style={{ display: "flex", gap: 16, width: "100%" }}>
+                <Form.Item name="category" rules={[{ required: true }]} noStyle>
+                  <Select placeholder="Kategoriya" style={{ width: "100%" }}>
+                    <Select.Option value="688cab0f66ac4bb5a48e7651">
+                      Ichimliklar
+                    </Select.Option>
+                  </Select>
+                </Form.Item>
+
+                <Form.Item
+                  name="subcategory"
+                  rules={[{ required: true }]}
+                  noStyle
+                >
+                  <Select placeholder="Subkategoriya" style={{ width: "100%" }}>
+                    <Select.Option value="688cac0c66ac4bb5a48e7655">
+                      Gazli ichimliklar
+                    </Select.Option>
+                  </Select>
+                </Form.Item>
+              </div>
+            </Form.Item>
+                  <Form.Item label="Qo'shimcha ma'lumotlar">
+              <Space.Compact style={{ width: "100%" }}>
+                <Input
+                  value={newAdditional}
+                  onChange={(e) => setNewAdditional(e.target.value)}
+                />
+                <Button type="primary" onClick={handleAddAdditional}>
+                  Qo'shish
+                </Button>
+              </Space.Compact>
+              <div style={{ marginTop: "8px" }}>
+                {additionals.map((item, index) => (
+                  <div
+                    key={index}
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      gap: "8px",
+                      marginTop: 4,
+                    }}
+                  >
+                    <span>{item}</span>
+                    <Button
+                      icon={<FaX />}
+                      size="small"
+                      type="text"
+                      danger
+                      onClick={() => handleRemoveAdditional(index)}
+                    />
+                  </div>
+                ))}
+              </div>
+            </Form.Item>
+            <Form.Item
+              style={{ gridColumn: "span 2", marginBottom: 0 }}
+              label={
+                <Checkbox
+                  checked={showNutrition}
+                  onChange={(e) => setShowNutrition(e.target.checked)}
+                >
+                  100 gr tarkibida
+                </Checkbox>
+              }
+            >
+              {showNutrition && (
+                <div style={{ display: "flex", gap: 16, width: "100%" }}>
+                  <Form.Item
+                    name={["nutritional_value", "kkal"]}
+                    rules={[{ type: "number" }]}
+                    noStyle
+                  >
+                    <InputNumber
+                      placeholder="Kkal"
+                      step={0.1}
+                      style={{ width: "100%" }}
+                    />
+                  </Form.Item>
+                  <Form.Item
+                    name={["nutritional_value", "protein"]}
+                    rules={[{ type: "number" }]}
+                    noStyle
+                  >
+                    <InputNumber
+                      placeholder="Oqsillar"
+                      step={0.1}
+                      style={{ width: "100%" }}
+                    />
+                  </Form.Item>
+                  <Form.Item
+                    name={["nutritional_value", "fat"]}
+                    rules={[{ type: "number" }]}
+                    noStyle
+                  >
+                    <InputNumber
+                      placeholder="Yog'lar"
+                      step={0.1}
+                      style={{ width: "100%" }}
+                    />
+                  </Form.Item>
+                  <Form.Item
+                    name={["nutritional_value", "uglevod"]}
+                    rules={[{ type: "number" }]}
+                    noStyle
+                  >
+                    <InputNumber
+                      placeholder="Uglevodlar"
+                      step={0.1}
+                      style={{ width: "100%" }}
+                    />
+                  </Form.Item>
+                </div>
+              )}
+            </Form.Item>
           </div>
-          <div>
-            <p>Birlik tavsifi</p>
-            <input type="text" name="unit_description" />
-          </div>
-          <div>
-            <p>Yaroqlilik kunlari</p>
-            <input type="number" name="expiration" />
-          </div>
-          <div>
-            <p>Sotish narxi</p>
-            <input type="number" name="selling_price" />
-          </div>
-        </div>
-        <br />
-        <div>
-          <div>
-            <p>Kategoriya</p>
-            <select name="category">
-              <option value="688cab0f66ac4bb5a48e7651">Ichimliklar</option>
-            </select>
-          </div>
-          <div>
-            <p>Subkategoriya</p>
-            <select name="subcategory">
-              <option value="688cac0c66ac4bb5a48e7655">
-                Gazli ichimliklar
-              </option>
-            </select>
-          </div>
-        </div>
-        <p>Qo'shimcha malumotlar</p>
-        <div>
-          <input
-            type="text"
-            value={newAdditional}
-            onChange={(e) => setNewAdditional(e.target.value)}
-          />
-          <button type="button" onClick={handleAddAdditional}>
-            Qo'shish
-          </button>
-          {additionals.map((item, index) => (
-            <p key={index}>
-              {item}{" "}
-              <button
-                type="button"
-                onClick={() => handleRemoveAdditional(index)}
+          <div className="product-add-grid">
+      
+
+            <Form.Item label="Rasmlar">
+              <Upload
+                listType="picture-card"
+                multiple
+                fileList={fileList}
+                beforeUpload={() => false}
+                onChange={handleFileChange}
               >
-                <FaX />
-              </button>
-            </p>
-          ))}
-        </div>
-        <br />
-        100 gr tarkibida
-        <div>
-          <div>
-            <p>Kkal</p>
-            <input type="number" step={0.1} name="nutritional_value.kkal" />
+                {fileList.length < 8 && (
+                  <div>
+                    <PlusOutlined />
+                    <div style={{ marginTop: 8 }}>Yuklash</div>
+                  </div>
+                )}
+              </Upload>
+            </Form.Item>
+
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                Kiritish
+              </Button>
+            </Form.Item>
           </div>
-          <div>
-            <p>Oqsillar</p>
-            <input type="number" step={0.1} name="nutritional_value.protein" />
-          </div>
-          <div>
-            <p>Yog'lar</p>
-            <input type="number" step={0.1} name="nutritional_value.fat" />
-          </div>
-          <div>
-            <p>Uglevodlar</p>
-            <input type="number" step={0.1} name="nutritional_value.uglevod" />
-          </div>
-        </div>
-        <p>Rasmlar</p>
-        <input type="file" name="image_log" multiple ref={imageRef} />
-        <button type="submit">Kiritish</button>
-      </form>
+        </Form>
+      </div>
     </div>
   );
 };
