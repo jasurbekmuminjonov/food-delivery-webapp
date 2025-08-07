f9918b938e8cef771c3da2197f8ee9dfa9c9c202
Jasur <j31548359@gmail.com>
2025-08-07 13:42:53 +0500

    :telegram

--- webapp_client/src/layout/Layout.jsx
+++ webapp_client/src/layout/Layout.jsx
@@ -1,7 +1,19 @@
+import { useEffect } from "react";
+
 const Layout = () => {
+  let telegramId;
+  useEffect(() => {
+    if (window.Telegram.WebApp) {
+      telegramId = window.Telegram.WebApp.initDataUnsafe?.user?.id;
+      if (telegramId) {
+        localStorage.setItem("telegram_id", telegramId);
+        console.log("Telegram ID:", telegramId);
+      }
+    }
+  }, []);
   return (
     <div className="layout">
-      <p>{localStorage.getItem("telegram_id")}</p>
+      <p>{telegramId}</p>
     </div>
   );
 };
