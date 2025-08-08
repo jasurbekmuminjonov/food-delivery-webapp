323285a8ead91f5407707edcf5586a96d17a1688
Jasur <j31548359@gmail.com>
2025-08-08 15:33:48 +0500

    fi

--- server/middlewares/basic.auth.js
+++ server/middlewares/basic.auth.js
@@ -3,32 +3,35 @@ const basicAuth = (req, res, next) => {
   if (publicPaths.includes(req.path)) {
     return next();
   }
+
   if (!req.headers.authorization?.startsWith("Basic ")) {
     return res
       .status(401)
       .json({ message: "Avtorizatsiya noto'g'ri formatda" });
   }
 
-  const credentials = req.headers.authorization?.split(" ")[1];
-  if (!credentials)
+  const credentials = req.headers.authorization.split(" ")[1];
+  if (!credentials) {
     return res
       .status(401)
       .json({ message: "Avtorizatsiya ma'lumotlari topilmadi" });
+  }
+
   try {
-    const decoded = Buffer.from(credentials, "base64").toString("utf8");
-    const data = JSON.parse(decoded);
-    if (!data.telegram_id) {
+    const decoded = Buffer.from(credentials, "base64").toString("utf8"); // "telegram_id:parol"
+    const [telegram_id] = decoded.split(":"); // faqat telegram_id olamiz
+
+    if (!telegram_id) {
       return res.status(401).json({
         message: "Avtorizatsiya ma'lumotlarida telegram_id mavjud emas",
       });
     }
 
-    req.user = {
-      telegram_id: data.telegram_id,
-    };
+    req.user = { telegram_id };
     next();
   } catch (err) {
     res.status(401).json({ message: "Avtorizatsiya ma'lumotlari noto'g'ri" });
   }
 };
+
 module.exports = basicAuth;
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -9,6 +9,8 @@ const {
   createUser,
   getUserByQuery,
 } = require("../controllers/user.controller");
+const { getProducts } = require("../controllers/product.controller");
+const { getCategories } = require("../controllers/category.controller");
 
 const rt = express.Router();
 
@@ -16,6 +18,10 @@ rt.post("/order/create", createOrder);
 rt.get("/order/user/get", getOrdersByUser);
 rt.get("/order/user/get/actual", getActualOrdersByUser);
 rt.put("/order/cancel", cancelOrder);
+rt.get("/product/get", getProducts);
+rt.get("/category/get", getCategories);
+
+
 
 rt.post("/user/create", createUser);
 rt.get("/user/get", getUserByQuery);
